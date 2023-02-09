5616380d6b1a121269dcf71ba0f1947692be017a
Chrob0 <76533304+Chrob0@users.noreply.github.com>
2023-02-09 10:32:07 +0100

    Add files via upload

---
+++ v6(miniCTF)/Programmering/decrypt.py
@@ -0,0 +1,3 @@
+alphabet = "ABCDEFGHIJKLMNOPQRTSVWXYZÅÄÖabcdefghijklmnopqrstuvwxyzåäö1234567890½§!@#£¤$%&/{([)]=}?+\`^¨~*';,:._-><|"
+key = "═↓îñ↑⌡╝¢Ç.w7vΘ;n0½ú└♥-I¡óO2pZäâ‼/u▒╕8ö↕]→♂r╠Q╔'╞o▓♣Æ[üª↨ÿÄC░<∟¿♀éÖ¬▬▲mû►Hàyìe◄_Uô`@3▼ïLGè5$acBPR♪x!•gd&iYEh?DFST46)K#9=,↔◘☺(1♫ ◙☼←☻○¶♠"
+text = "¢↑I_â╕Ä╠╕/╔!8♂!╠[╞╕¬♂▼"
