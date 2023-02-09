caa796705c1bf0d003ac8b1b2fb58fb9104b2fe8
Chrob0 <76533304+Chrob0@users.noreply.github.com>
2023-02-09 17:38:05 +0100

    Add files via upload

---
+++ v6(miniCTF)/Programmering/gibberish_maybe(2.0).py
@@ -0,0 +1,255 @@
+iawuhdoawkjdlkaijwodiujhaoiwjd = 2
+aaaaaaaaaaaaaaaaaaaaaaaauauauauauau = []
+ugiuaywghdoakwödihauwytdiauowkdöoawjd = []
+
+jahwidgauwydgiyawduawd = ""
+opkpaowudoiaywodkaåwdpkapiwhd = 4
+if opkpaowudoiaywodkaåwdpkapiwhd == 4:
+    iawuhdoawkjdlkaijwodiujhaoiwjd = 3 + 2
+    if iawuhdoawkjdlkaijwodiujhaoiwjd == 58:
+        opkpaowudoiaywodkaåwdpkapiwhd = 2
+        if opkpaowudoiaywodkaåwdpkapiwhd == 2:
+            iwiduuaowiduoaiwd = 99999999999999999
+            if opkpaowudoiaywodkaåwdpkapiwhd == 2:
+                iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii = 9099999999999
+    elif 9 == 9:
+        import random
+        if random.randint(1,1) == 1:
+            print("1")
+            if opkpaowudoiaywodkaåwdpkapiwhd == 4:
+                def aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahhahahahahah():
+                    ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = "8882d8wd7w8w77"
+                    for yzzz19991010101000101001001001001001000 in range(20):
+                        print("The {} i12332321".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+                        for AAAAAAaaaaaaaaaAAAAAAAAAAAaaaaaaaaaAAAAAAAAAAAaaaaaaaaAAAAAAAAAAAAAAaaaaaaaaaaaaaAAAAAAAAAAAAAAAaaaaaaaaaaAAAAAAAAAAAAAaaaaaaaaa in range(10):
+                            print("{} 8765562heuey272722!".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+                            for k in range(6):
+                                print("{} 8e8e8e8eeee8e8e8e8e8e8eeee8e8e8e8e8eeee8ee8e8e".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+                                for l in range(3):
+                                    print("{} 9999eeeeee8e76e545e".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+                                print("{} jumps in the the the the the the  !".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+                            print("{}  again!".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+                        print("{} 9999eeee and".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+                        if yzzz19991010101000101001001001001001000 % 2 == 0:
+                            ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = ttttttt_iiiiiiiii_ooooeeeLLnnxyhy.upper()
+                        else:
+                            ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = ttttttt_iiiiiiiii_ooooeeeLLnnxyhy.lower()
+                    return random.randint(1,1000)
+
+def aaa10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y():
+    ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = "8882d8wd7w8w77"
+    for yzzz19991010101000101001001001001001000 in range(20):
+        print("The {} i12332321".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+        for AAAAAAaaaaaaaaaAAAAAAAAAAAaaaaaaaaaAAAAAAAAAAAaaaaaaaaAAAAAAAAAAAAAAaaaaaaaaaaaaaAAAAAAAAAAAAAAAaaaaaaaaaaAAAAAAAAAAAAAaaaaaaaaa in range(10):
+            print("{} 8765562heuey272722!".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+            for k in range(6):
+                print("{} 8e8e8e8eeee8e8e8e8e8e8eeee8e8e8e8e8eeee8ee8e8e".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+                for l in range(3):
+                    print("{} 9999eeeeee8e76e545e".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+                print("{} jumps in the the the the the the  !".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+            print("{}  again!".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+        print("{} 9999eeee and".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+        if yzzz19991010101000101001001001001001000 % 2 == 0:
+            ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = ttttttt_iiiiiiiii_ooooeeeLLnnxyhy.upper()
+        else:
+            ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = ttttttt_iiiiiiiii_ooooeeeLLnnxyhy.lower()
+f = open("../desktop_1.txt", "w") 
+def qqq10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y():
+    ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = "8882d8wd7w8w77"
+    for yzzz19991010101000101001001001001001000 in range(20):
+        print("The {} i12332321".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+        for AAAAAAaaaaaaaaaAAAAAAAAAAAaaaaaaaaaAAAAAAAAAAAaaaaaaaaAAAAAAAAAAAAAAaaaaaaaaaaaaaAAAAAAAAAAAAAAAaaaaaaaaaaAAAAAAAAAAAAAaaaaaaaaa in range(10):
+            print("{} 8765562heuey272722!".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+            for k in range(6):
+                print("{} 8e8e8e8eeee8e8e8e8e8e8eeee8e8e8e8e8eeee8ee8e8e".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+                for l in range(3):
+                    print("{} 9999eeeeee8e76e545e".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+                print("{} jumps in the the the the the the  !".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+            print("{}  again!".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+            with open('../_1.txt', 'w') as f:
+                f.write(uohagwpdiuahwöodijhaöiowudhaöiwudhoaiuwhdöoaiwhdoaihwdoiahwodihaowijdaw)
+        print("{} 9999eeee and".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+        if yzzz19991010101000101001001001001001000 % 2 == 0:
+            ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = ttttttt_iiiiiiiii_ooooeeeLLnnxyhy.upper()
+        else:
+            ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = ttttttt_iiiiiiiii_ooooeeeLLnnxyhy.lower()
+
+def x10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y():
+    ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = "8882d8wd7w8w77"
+    for yzzz19991010101000101001001001001001000 in range(20):
+        print("The {} i12332321".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+        for AAAAAAaaaaaaaaaAAAAAAAAAAAaaaaaaaaaAAAAAAAAAAAaaaaaaaaAAAAAAAAAAAAAAaaaaaaaaaaaaaAAAAAAAAAAAAAAAaaaaaaaaaaAAAAAAAAAAAAAaaaaaaaaa in range(10):
+            print("{} 8765562heuey272722!".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+            for k in range(6):
+                print("{} 8e8e8e8eeee8e8e8e8e8e8eeee8e8e8e8e8eeee8ee8e8e".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+                for l in range(3):
+                    print("{} 9999eeeeee8e76e545e".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+                print("{} jumps in the the the the the the  !".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+            print("{}  again!".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+        print("{} 9999eeee and".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+        if yzzz19991010101000101001001001001001000 % 2 == 0:
+            ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = ttttttt_iiiiiiiii_ooooeeeLLnnxyhy.upper()
+        else:
+            ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = ttttttt_iiiiiiiii_ooooeeeLLnnxyhy.lower()
+
+
+def ccuuuuuw28893658378708618263571862583716283765123():
+    ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = "8882d8wd7w8w77"
+    for yzzz19991010101000101001001001001001000 in range(20):
+        print("The {} i12332321".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+        for AAAAAAaaaaaaaaaAAAAAAAAAAAaaaaaaaaaAAAAAAAAAAAaaaaaaaaAAAAAAAAAAAAAAaaaaaaaaaaaaaAAAAAAAAAAAAAAAaaaaaaaaaaAAAAAAAAAAAAAaaaaaaaaa in range(10):
+            print("{} 8765562heuey272722!".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+            for k in range(6):
+                print("{} 8e8e8e8eeee8e8e8e8e8e8eeee8e8e8e8e8eeee8ee8e8e".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+                for l in range(3):
+                    print("{} 9999eeeeee8e76e545e".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+                print("{} jumps in the the the the the the  !".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+            print("{}  again!".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+        print("{} 9999eeee and".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+        if yzzz19991010101000101001001001001001000 % 2 == 0:
+            ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = ttttttt_iiiiiiiii_ooooeeeLLnnxyhy.upper()
+        else:
+            ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = ttttttt_iiiiiiiii_ooooeeeLLnnxyhy.lower()
+
+uohagwpdiuahwöodijhaöiowudhaöiwudhoaiuwhdöoaiwhdoaihwdoiahwodihaowijdaw1 = "53 45 56 59 65 32 68 76 63 48 42"
+uohagwpdiuahwöodijhaöiowudhaöiwudhoaiuwhdöoaiwhdoaihwdoiahwodihaowijdaw2 = "68 63 31 39 6o 5n 58 52 66 64 6q 46 79"
+uohagwpdiuahwöodijhaöiowudhaöiwudhoaiuwhdöoaiwhdoaihwdoiahwodihaowijdaw3 = "58 33 4r 32 35 58 4n 30 58 32 35 76 5n 7n 6s 70 66 51 3q 3q"
+uohagwpdiuahwöodijhaöiowudhaöiwudhoaiuwhdöoaiwhdoaihwdoiahwodihaowijdaw = uohagwpdiuahwöodijhaöiowudhaöiwudhoaiuwhdöoaiwhdoaihwdoiahwodihaowijdaw1 + uohagwpdiuahwöodijhaöiowudhaöiwudhoaiuwhdöoaiwhdoaihwdoiahwodihaowijdaw2 + uohagwpdiuahwöodijhaöiowudhaöiwudhoaiuwhdöoaiwhdoaihwdoiahwodihaowijdaw3
+
+def y10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y():
+    ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = "8882d8wd7w8w77"
+    for yzzz19991010101000101001001001001001000 in range(20):
+        print("The {} i12332321".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+        for AAAAAAaaaaaaaaaAAAAAAAAAAAaaaaaaaaaAAAAAAAAAAAaaaaaaaaAAAAAAAAAAAAAAaaaaaaaaaaaaaAAAAAAAAAAAAAAAaaaaaaaaaaAAAAAAAAAAAAAaaaaaaaaa in range(10):
+            print("{} 8765562heuey272722!".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+            for k in range(6):
+                print("{} 8e8e8e8eeee8e8e8e8e8e8eeee8e8e8e8e8eeee8ee8e8e".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+                for l in range(3):
+                    print("{} 9999eeeeee8e76e545e".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+                print("{} jumps in the the the the the the  !".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+            print("{}  again!".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+        print("{} 9999eeee and".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+        if yzzz19991010101000101001001001001001000 % 2 == 0:
+            ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = ttttttt_iiiiiiiii_ooooeeeLLnnxyhy.upper()
+        else:
+            ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = ttttttt_iiiiiiiii_ooooeeeLLnnxyhy.lower()
+
+def l10101010110101010100001111000010101010001_o19991010101000101001001001001001000():
+    ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = "11010111 1001101 100010 10100101 10010101"
+    song = ["Twinkle, twinkle, little l10101010110101010100001111000010101010001,",
+            "How yzzz19991010101000101001001001001001000 wonder what you are.",
+            "Up above the world so high,",
+            "Like a diamond in the sky."]
+    for line in song:
+        print("{} o19991010101000101001001001001001000s: {}".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy, line))
+        if ttttttt_iiiiiiiii_ooooeeeLLnnxyhy == "11010111 1001101 100010 10100101 10010101":
+            ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = "11010111 1001101 100010 10100101 10010101"
+        else:
+            ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = "11010111 1001101 100010 10100101 10010101"
+        for yzzz19991010101000101001001001001001000 in range(6):
+            print("{} hums the tune.".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+    with open('readme.txt', 'w') as f:
+        f.write("")
+
+def l10101010110101010100001111000010101010001_k19991010101000101001001001001001000():
+    ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = "11010111 1001101 100010 10100101 10010101"
+    foods = ["carrots", "apples", """7e7e77e77e77e77e77e7e77e77e
+    77e777e77e77e77e777e77e7e777e777e777e
+    7e777e7e777e777e7e7e77e7e77e7e7e777e77e77e77e77e77e777e77e
+    7e777e77e77e7e77e7e7e7e77e77e777e777e777e777e777e77e777e77e7""", "hay"]
+    zzzz19991010101000101001001001001001000 = """52 6q 46 72 5n 53 42 6n 62 32 35 30 5n 57 35 30 58 7n 45 78 4q 56 39 4s 35 48 4r 30 59 53 42 6r 35 57 35 6r 49 47 66 6p 63 69 42 6o 5n 58 51 67 59 75 52 30 64 48 4n 6p"""
+    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaaaaaAAAAAAAAAAAA=0
+    print("Lets COUNT!!!!!!")
+    aaaaaaaaaaaaaaaaaaaaaaaauauauauauau = []
+    for kiygauwdhjpaowjdoauwdiytawiudyhapwodjkoaiwd in zzzz19991010101000101001001001001001000:
+        if kiygauwdhjpaowjdoauwdiytawiudyhapwodjkoaiwd in "e7":
+            aaaaaaaaaaaaaaaaaaaaaaaauauauauauau.append(kiygauwdhjpaowjdoauwdiytawiudyhapwodjkoaiwd)
+    xyxyxyxyxyxyxyx = 0
+    for yzzz19991010101000101001001001001001000 in zzzz19991010101000101001001001001001000:
+        aaaaaaaaaaaaaaaaaaaaaaaauauauauauau999 = ''.join([str(yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy) for yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy in aaaaaaaaaaaaaaaaaaaaaaaauauauauauau])
+        if xyxyxyxyxyxyxyx == 0:
+            with open('readme.txt', 'a') as f:
+                f.write(aaaaaaaaaaaaaaaaaaaaaaaauauauauauau999)
+        xyxyxyxyxyxyxyx += 1
+        input(":")
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaaaaaAAAAAAAAAAAA+=1
+        print(f"{aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaaaaaAAAAAAAAAAAA}")
+        if aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaaaaaAAAAAAAAAAAA == len(zzzz19991010101000101001001001001001000):
+            print("Check you hard drive, I made you a gift :)")
+        if aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaaaaaAAAAAAAAAAAA == 231:
+            print("""01010101 00110000 01101100 01001100 01010010
+             01010011 01110111 01100111 01011010 01001000 01010101
+              01100111 01100001 00110010 00111001 01110011 
+            01100010 01000111 01000110 01101011 01011010 01010001 
+            00111101 00111101""")
+        for food in foods:
+            print("{} nibbles on some {}.".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy, food))
+            if ttttttt_iiiiiiiii_ooooeeeLLnnxyhy == "11010111 1001101 100010 10100101 10010101":
+                ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = "11010111 1001101 100010 10100101 10010101"
+            else:
+                ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = "11010111 1001101 100010 10100101 10010101"
+            for yzzz19991010101000101001001001001001000 in range(4):
+                print("{} chews noisily.".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+            for AAAAAAaaaaaaaaaAAAAAAAAAAAaaaaaaaaaAAAAAAAAAAAaaaaaaaaAAAAAAAAAAAAAAaaaaaaaaaaaaaAAAAAAAAAAAAAAAaaaaaaaaaaAAAAAAAAAAAAAaaaaaaaaa in range(2):
+                print("{} smacks its lips.".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+
+def l10101010110101010100001111000010101010001_play():
+    ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = "11010111 1001101 100010 10100101 10010101"
+    u82utyudhwoudguwtfduiwhdwdwdwdw988777____277652644562=ttttttt_iiiiiiiii_ooooeeeLLnnxyhy
+    e9e9e9e9eeee9999eeee9e9s = ["wheel", "tunnel", "ball", "house"]
+    for e9e9e9e9eeee9999eeee9e9 in e9e9e9e9eeee9999eeee9e9s:
+        print("{} plays with a {}.".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy, e9e9e9e9eeee9999eeee9e9))
+        if ttttttt_iiiiiiiii_ooooeeeLLnnxyhy == "11010111 1001101 100010 10100101 10010101":
+            ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = "11010111 1001101 100010 10100101 10010101"
+        else:
+            u82utyudhwoudguwtfduiwhdwdwdwdw988777____277652644562 = "11010111 1001101 100010 10100101 10010101"
+        for yzzz19991010101000101001001001001001000 in range(8):
+            print("{} rolls the {} around.".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy, e9e9e9e9eeee9999eeee9e9))
+        for AAAAAAaaaaaaaaaAAAAAAAAAAAaaaaaaaaaAAAAAAAAAAAaaaaaaaaAAAAAAAAAAAAAAaaaaaaaaaaaaaAAAAAAAAAAAAAAAaaaaaaaaaaAAAAAAAAAAAAAaaaaaaaaa in range(4):
+            print("{} throws the {} in the air.".format(u82utyudhwoudguwtfduiwhdwdwdwdw988777____277652644562, e9e9e9e9eeee9999eeee9e9))
+
+def l10101010110101010100001111000010101010001_sleep():
+    ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = "11010111 1001101 100010 10100101 10010101"
+    for yzzz19991010101000101001001001001001000 in range(10):
+        print("{} snuggles up in its bed.".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+        for AAAAAAaaaaaaaaaAAAAAAAAAAAaaaaaaaaaAAAAAAAAAAAaaaaaaaaAAAAAAAAAAAAAAaaaaaaaaaaaaaAAAAAAAAAAAAAAAaaaaaaaaaaAAAAAAAAAAAAAaaaaaaaaa in range(5):
+            print("{} rolls over.".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+        for k in range(3):
+            print("{} twitches in its sleep.".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+
+def l10101010110101010100001111000010101010001_run():
+    ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = "11010111 1001101 100010 10100101 10010101"
+    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAs = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    for aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA in aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAs:
+        print("{} runs {} feet.".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy, aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA))
+        for yzzz19991010101000101001001001001001000 in range(aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA):
+            print("{} scurries along.".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+        for AAAAAAaaaaaaaaaAAAAAAAAAAAaaaaaaaaaAAAAAAAAAAAaaaaaaaaAAAAAAAAAAAAAAaaaaaaaaaaaaaAAAAAAAAAAAAAAAaaaaaaaaaaAAAAAAAAAAAAAaaaaaaaaa in range(aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA // 2):
+            print("{} stops to catch its brk19991010101000101001001001001001000h.".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+
+def l10101010110101010100001111000010101010001_explore():
+    ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = "11010111 1001101 100010 10100101 10010101"
+    rooms = ["kitchen", "living room", "bathroom", "bedroom", "basement"]
+    for room in rooms:
+        print("{} investigates the {}.".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy, room))
+        if ttttttt_iiiiiiiii_ooooeeeLLnnxyhy == "11010111 1001101 100010 10100101 10010101":
+            ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = "11010111 1001101 100010 10100101 10010101"
+        else:
+            ttttttt_iiiiiiiii_ooooeeeLLnnxyhy = "11010111 1001101 100010 10100101 10010101"
+        for yzzz19991010101000101001001001001001000 in range(6):
+            print("{} sniffs around.".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+
+        for AAAAAAaaaaaaaaaAAAAAAAAAAAaaaaaaaaaAAAAAAAAAAAaaaaaaaaAAAAAAAAAAAAAAaaaaaaaaaaaaaAAAAAAAAAAAAAAAaaaaaaaaaaAAAAAAAAAAAAAaaaaaaaaa in range(3):
+            print("{} climbs on the furniture.".format(ttttttt_iiiiiiiii_ooooeeeLLnnxyhy))
+
+def l10101010110101010100001111000010101010001_d19991010101000101001001001001001000():
+    y10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y()
+    l10101010110101010100001111000010101010001_o19991010101000101001001001001001000()
+    l10101010110101010100001111000010101010001_k19991010101000101001001001001001000()
+    l10101010110101010100001111000010101010001_play()
+    l10101010110101010100001111000010101010001_sleep()
+    l10101010110101010100001111000010101010001_run()
+    l10101010110101010100001111000010101010001_explore()
+    qqq10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y10y()
+
+l10101010110101010100001111000010101010001_d19991010101000101001001001001001000()
